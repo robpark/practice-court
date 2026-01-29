6e8e98578dc8f55cf5a9fc4bb3eb5a025a1436e7
Rob Park <robert.park@4legssoftware.com>
2026-01-29 11:17:57 -0500

    Implement solution for "Smallest Missing Positive" challenge in JavaScript

--- codility/smallest-missing-positive/solution.js
+++ codility/smallest-missing-positive/solution.js
@@ -1,4 +1,20 @@
-export function solution(_inputArray) {
-    // TODO: implement
-    return 0;
+export function solution(inputArray) {
+    let smallest = 1
+
+    for (let index = 0; index < inputArray.length; index++) {
+        for (let index = 0; index < inputArray.length; index++) {
+            if (inputArray[index] < 1) continue;
+
+            if (inputArray[index] === smallest) {
+                smallest++;
+                break;
+            }
+        }
+    }
+
+    return smallest;
 }
+
+console.log(solution([1, 3, 6, 4, 1, 2])); // 5
+console.log(solution([1, 2, 3]));       // 4
+console.log(solution([-1, -3]));       // 1
