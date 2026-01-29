adcb5b92e3031db11b2eb8902d737ef661b6763c
Rob Park <robert.park@4legssoftware.com>
2026-01-29 11:28:05 -0500

    Refactor "Smallest Missing Positive" solution to improve readability and efficiency using a `Set`

--- codility/smallest-missing-positive/solution.js
+++ codility/smallest-missing-positive/solution.js
@@ -1,18 +1,21 @@
 export function solution(inputArray) {
-    let smallest = 1
+
+    let positives = new Set()
 
     for (let index = 0; index < inputArray.length; index++) {
-        for (let index = 0; index < inputArray.length; index++) {
-            if (inputArray[index] < 1) continue;
-
-            if (inputArray[index] === smallest) {
-                smallest++;
-                break;
-            }
+        if (inputArray[index] > 0) {
+            positives.add(inputArray[index])
         }
     }
 
-    return smallest;
+    if (positives.size < 1) return 1;
+
+    let result = 1
+    while (positives.has(result)) {
+        result++;
+    }
+
+    return result;
 }
 
 console.log(solution([1, 3, 6, 4, 1, 2])); // 5
