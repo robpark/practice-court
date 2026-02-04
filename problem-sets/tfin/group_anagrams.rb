4380cd1b6df46c3265360dde396d643ce950b94c
Rob Park <robert.park@4legssoftware.com>
2026-02-04 12:06:25 -0500

    Add specs configuration and refactor `group_anagrams` implementation

--- problem-sets/tfin/group_anagrams.rb
+++ problem-sets/tfin/group_anagrams.rb
@@ -1,3 +1,19 @@
 def group_anagrams(words)
-  []
+  return [] if words.empty?
+  return [words] if words.size == 1
+
+  sets = {}
+
+  # char_sets = words.map do |word|
+  #   word.chars.sort
+  # end
+  # char_sets.uniq!
+
+  words.each do |word|
+    key = word.downcase.chars.sort
+    sets[key] = [] unless sets.has_key?(key)
+    sets[key] << word
+  end
+
+  sets.values
 end
