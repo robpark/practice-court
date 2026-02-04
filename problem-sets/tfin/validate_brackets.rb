f686d41f8f33e1da3bf13636e7c2ac90abea5ee4
Rob Park <robert.park@4legssoftware.com>
2026-02-04 12:41:35 -0500

    Add `validate_brackets` implementation and specs configuration

--- problem-sets/tfin/validate_brackets.rb
+++ problem-sets/tfin/validate_brackets.rb
@@ -1,3 +1,15 @@
+def matching_pair(open, close)
+  open == '(' && close == ')' || open == '[' && close == ']' || open == '{' && close == '}'
+end
+
 def validate_brackets(str)
-  # ...
+  stack = []
+  str.each_char do |char|
+    if matching_pair(stack.last, char)
+      stack.pop
+    elsif '([{}])'.index(char)
+      stack.push char
+    end
+  end
+  stack.empty?
 end
