2f68b14d9df32ad8ff035ffcbbd7907a44d9cb5d
Rob Park <robert.park@4legssoftware.com>
2026-02-04 10:43:58 -0500

    clean

--- problem-sets/tfin/compress_ranges.rb
+++ problem-sets/tfin/compress_ranges.rb
@@ -1,33 +1,3 @@
 def compress_ranges(nums)
-  return '' if nums.empty?
-  return nums[0].to_s if nums.size == 1
 
-  result = []
-  range_start = nums[0]
-  range_end = range_start
-  for num in nums do
-    if num == range_start
-      # range_end = range_start
-      next
-    end
-    if num == range_end + 1
-      range_end = num
-    elsif range_start == range_end
-      result << range_start
-      range_start = num
-      range_end = num
-    end
-  end
-  if range_end > range_start
-    if range_end == range_start + 1
-      result << range_start << range_end
-    else
-      result << (range_start..range_end)
-    end
-  else
-    result << range_start
-  end
-
-  string_result  = result.join ','
-  string_result.sub('..', '-')
 end
