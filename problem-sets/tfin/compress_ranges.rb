fe9acb4078c25d847e45077f098ede0c9af3ee57
Rob Park <robert.park@4legssoftware.com>
2026-02-04 11:19:20 -0500

    check 1

--- problem-sets/tfin/compress_ranges.rb
+++ problem-sets/tfin/compress_ranges.rb
@@ -6,9 +6,30 @@ def compress_ranges(nums)
   normalized = nums.sort.uniq
 
   # ranges
-  # i=0
-  # while i <
+  ranges = []
+  i = 0
+  while i < normalized.size
+    start = i
+    j = i
+    while j + 1 < normalized.size && normalized[j] + 1 == normalized[j + 1]
+      j += 1
+    end
+    ranges << [i, j]
+    i = j + 1
+  end
+
+  result = []
+  ranges.each do |range|
+    if normalized[range.last] == normalized[range.first]
+      result << normalized[range.first]
+    elsif normalized[range.last] == normalized[range.first] + 1
+      result << normalized[range.first].to_s + ',' + normalized[range.last].to_s
+    else
+      result << normalized[range.first].to_s + '-' + normalized[range.last].to_s
+    end
+  end
 
   # format
-  normalized.join ','
+  # normalized.join(  ',')
+  result.join(',')
 end
